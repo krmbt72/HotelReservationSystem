e22425a229d886c526443cf685bebcc756adf942
Uğur ÖZKAN <bilgivecesaret@gmail.com>
2023-11-28 23:45:03 +0300

    login page created
    Only personnel authorized by the hotel management can access the program.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,38 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QMessageBox>
+#include <QPixmap>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QPixmap iconImage(":/resource/icon/password.png");
+    int w = ui->label_icon->width();
+    int h = ui->label_icon->height();
+    ui->label_icon->setPixmap(iconImage.scaled(w,h,Qt::KeepAspectRatio));
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    QString username = ui->lineEdit_username->text();
+    QString password = ui->lineEdit_password->text();
+    if(username == "test" && password == "123"){
+        QMessageBox::information(this,"Login","Username and password is correct");
+        hide();
+        homePage = new HomePage(this);
+        homePage->show();
+        ui->statusbar->showMessage("Welcome",3000);
+    } else {
+        QMessageBox::warning(this,"Login","Username or password is not correct");
+        ui->statusbar->showMessage("invalid value",3000);
+    }
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "HomePage.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +16,11 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_pushButton_clicked();
+
 private:
     Ui::MainWindow *ui;
+    HomePage *homePage;
 };
 #endif // MAINWINDOW_H
